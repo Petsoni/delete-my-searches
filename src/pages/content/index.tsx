776cb3a5a2c7297680ce04b685464ec79841a50a
Petar Marković <pmarkovic2308@gmail.com>
2025-11-07 16:14:41 +0100

    Clear history and cookies

--- src/pages/content/index.tsx
+++ src/pages/content/index.tsx
@@ -8,8 +8,8 @@ const rootContainer = document.querySelector('#__root');
 if (!rootContainer) throw new Error("Can't find Content root element");
 const root = createRoot(rootContainer);
 root.render(
-  <div className='absolute bottom-0 left-0 text-lg text-black bg-amber-400 z-50'  >
-    content script <span className='your-class'>loaded</span>
+  <div className='absolute bottom-0 left-0 text-lg text-black bg-red-500 z-50'>
+    Dev mode <span className='your-class'>DeleteMySearches</span>
   </div>
 );
 
--- src/pages/popup/Popup.tsx
+++ src/pages/popup/Popup.tsx
@@ -1,24 +1,45 @@
 import React from 'react';
-import logo from '@assets/img/logo.svg';
 
 export default function Popup() {
-  return (
-    <div className="absolute top-0 left-0 right-0 bottom-0 text-center h-full p-3 bg-gray-800">
-      <header className="flex flex-col items-center justify-center text-white">
-        <img src={logo} className="h-36 pointer-events-none animate-spin-slow" alt="logo" />
-        <p>
-          Edit <code>src/pages/popup/Popup.jsx</code> and save to reload.
-        </p>
-        <a
-          className="text-blue-400"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React!
-        </a>
-        <p>Popup styled with TailwindCSS!</p>
-      </header>
-    </div>
-  );
+
+    const deleteHistory = () => {
+        const numberOfHoursAgo = Date.now() - 4 * 60 * 60 * 1000;
+        chrome.history.deleteRange({startTime: numberOfHoursAgo, endTime: Date.now()}, () => {
+            clearCookies();
+        })
+    }
+
+    function clearCookies() {
+        chrome.cookies.getAll({}, (cookies) => {
+            cookies.forEach(cookie => {
+                chrome.cookies.remove({url: "https://" + cookie.domain, name: cookie.name});
+                closeAllTabs();
+            })
+        })
+    }
+
+    async function closeAllTabs() {
+        const allTabs = await chrome.tabs.query({currentWindow: true})
+        await chrome.tabs.remove(allTabs.map((tab) => {
+            return tab.id!;
+        }))
+    }
+
+    return (
+        <div className="absolute top-0 left-0 right-0 bottom-0 text-center h-full rounded-xl p-3 bg-white shadow-lg">
+            <header className="flex flex-col gap-4 font-semibold items-center justify-between h-full text-gray-900">
+                <div className="flex flex-col items-center justify-center gap-2">
+                    <h1 className={"text-2xl"}>Was I here?</h1>
+                    <h4 className="text-gray-500">
+                        One button to erase everything
+                    </h4>
+                </div>
+                <button
+                    onClick={() => deleteHistory()}
+                    className={"bg-red-500 hover:bg-red-700 w-full cursor-pointer text-white font-bold py-5 px-4 rounded-xl"}>
+                    Remove me from this browser
+                </button>
+            </header>
+        </div>
+    );
 }
